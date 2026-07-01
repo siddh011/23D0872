920d3fd1c29b6b960884bc29e5e26ef8c8b83964
siddh011 <167894511+siddh011@users.noreply.github.com>
2026-07-01 16:08:04 +0530

    ARM

---
+++ build/bme_ros2_simple_arm_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/siddharth/sor_ws1/Session5/bme_ros2_simple_arm_moveit_config
+# Build directory: /home/siddharth/sor_ws1/build/bme_ros2_simple_arm_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
