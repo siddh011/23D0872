2a496521189b09cf610d149ce774e82e323adf32
siddh011 <167894511+siddh011@users.noreply.github.com>
2026-07-01 16:25:30 +0530

    ARM

---
+++ build/bme_ros2_simple_arm_moveit_config/CMakeFiles/bme_ros2_simple_arm_moveit_config_uninstall.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for custom commands dependencies management for bme_ros2_simple_arm_moveit_config_uninstall.
---
+++ install/bme_ros2_simple_arm_moveit_config/share/bme_ros2_simple_arm_moveit_config/launch/move_group.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_move_group_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("mogi_arm", package_name="bme_ros2_simple_arm_moveit_config").to_moveit_configs()
+    return generate_move_group_launch(moveit_config)
